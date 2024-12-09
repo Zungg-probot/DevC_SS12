58a68f838e78b3e5400522f29e21bb3451f87924
Zungg-probot <zunggmlem@gmail.com>
2024-12-09 16:11:54 +0700

    Update SS12_BT6.c

--- SS12_BT6.c
+++ SS12_BT6.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
-
-int soNguyento(int n){
-    if(n <= 1){
-        return 0;
-    }
-    for(int i = 2; i*i<=n;i++){
-        if(n % i == 0){
-            return 0;
+int soHoanHao(int n){
+    if (n <= 0){
+    	return 0;
+	}
+    int sum = 0;
+    for(int i = 1;i < n; i++){
+        if (n % i == 0){
+        sum += i;
         }
     }
-    return 1;
+    if(sum == n){
+    	return n;
+	}
+    return 0;
 }
 int main(){
-    int num1, num2;
-
-    printf("Nhap vao so thu nhat: ");
+    int num1,num2;
+    printf("Nhap so thu nhat: ");
     scanf("%d", &num1);
-    printf("Nhap vao so thu hai: ");
+    printf("Nhap so thu hai: ");
     scanf("%d", &num2);
-    if (soNguyento(num1)){
-        printf("%d la so nguyen to\n",num1);
+    if(soHoanHao(num1)){
+        printf("%d la so hoan hao\n", num1);
     }else{
-        printf("%d khong phai la so nguyen to\n",num1);
+        printf("%d khong phai so hoan hao\n", num1);
     }
-    if (soNguyento(num2)){
-        printf(" %d la so nguyen to\n",num2);
+    if(soHoanHao(num2)){
+        printf("%d la so hoan hao\n", num2);
     }else{
-        printf("%d khong phai la so nguyen to\n",num2);
+        printf("%d khong phai so hoan hao\n", num2);
     }
     return 0;
 }
